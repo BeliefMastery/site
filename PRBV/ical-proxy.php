78aea4586974ea2a6d543a0294f8aa5a5b419128
Warwick Marshall <beliefmastery.mail@gmail.com>
2025-09-24 13:14:09 +1200

    Update ical-proxy.php

--- PRBV/ical-proxy.php
+++ PRBV/ical-proxy.php
@@ -1,13 +1,12 @@
 <?php
-// Very simple proxy for ICS files
+// Minimal iCal proxy to bypass CORS
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: text/calendar");
 
-// validate & pass through
 if (isset($_GET['url'])) {
     $url = urldecode($_GET['url']);
-    // basic sanity check: must start with Beds24 domain
-    if (strpos($url, "api.beds24.com") === 0 || strpos($url, "https://api.beds24.com") === 0) {
+    // only allow .ics files
+    if (preg_match('/^https:\\/\\/.+\\.ics$/i', $url)) {
         echo file_get_contents($url);
     } else {
         http_response_code(403);
